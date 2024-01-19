09d02081f2298869d9c19ec6f98afec92f90dc6c
Michael Geuze <michael.geuze@gmail.com>
2024-01-19 22:06:49 +0100

    Changed a few things in the architecture. cmd_handler is now the orchestrating logic between database and mqtt data

--- modules/cmd_handler.py
+++ modules/cmd_handler.py
@@ -1,24 +1,72 @@
 import os
 import json
-from modules.mqtt_handler import MQTT_handler   
+import paho.mqtt.client as mqtt
+from paho.mqtt.client import MQTT_ERR_SUCCESS, MQTT_ERR_NO_CONN
 
-class Command_Handler():
-    def __init__(self, mqtt_handler: MQTT_handler, command_list_filename="commandlist.json") -> None:
-        self.mqtt_handler = mqtt_handler
-        self.command_list_filename = command_list_filename
+class MQTTCommandHandler():
+    def __init__(self, command_list_filename="commandlist.json", passes_filename="passes\\network.pw") -> None:
         self.current_path = os.path.dirname(__file__)
+        self.passes_filename = passes_filename  # File containing the mqtt host
+        self.command_list_filename = command_list_filename
         self.commands = self._get_commands()
+
+        self.client = mqtt.Client()
+        self.client.on_connect = self._on_connect
+        self.client.on_message = self._on_message
+        #self.client.message_callback_add("sensor/+/data", self._on_sensor_data)
+        self.client.message_callback_add("sensor/+/data/#", self._on_sensor_data)
+        self.mqtt_host = self._get_mqtt_host()
+        self.client.connect(self.mqtt_host, 1883, 60)
+
+        self.client.loop_start()
+
+    def __del__(self):
+        self.client.loop_stop()
+
+    def _on_message(self, client, userdata, message):
+        print(f"received {message.topic} {str(message.payload)}")
+
+
+    def _on_sensor_data(self, client, userdata, message):
+        
+        sensor_id = message.topic.split('/')[1]
+        data = message.payload
+        print(f"Sensor {sensor_id} data: {data}")
+
+    def _get_mqtt_host(self):
+        try:
+            with open(os.path.join(self.current_path, self.passes_filename), 'r') as file:
+                mqtt_host = json.load(file)['mqtt_host']
+        except Exception as e:
+            print(f"An error occurred reading mqtt host from json file: {e}")
+            return None
+        return mqtt_host
 
     def _get_commands(self):
         try:
             with open(os.path.join(self.current_path, self.command_list_filename), 'r') as file:
                 commands = json.load(file)['commands']
         except Exception as e:
-            print(f"An error occurred reading mqtt host from json file: {e}")
+            print(f"An error occurred reading command list from json file: {e}")
+            return None
         return commands
     
+    def _on_connect(self, client, userdata, flags, rc):
+        print(f"Connected to mqtt broker with result code {rc}")
+        self.client.subscribe("sensor/#") 
+        print("Subscribed to sensor/#")
+
+    def publish(self, topic, message):
+        self.client.publish(topic, message)
+
     def invoke_humidity_measurment(self, sensor_id):
-        self.mqtt_handler.publish(f'sensor/{sensor_id}/cmd', self.commands['CMD_GET_HUMIDITY_DATA'])
+        self.client.publish(f'sensor/{sensor_id}/cmd', self.commands['CMD_GET_HUMIDITY_DATA'])
 
     def invoke_temperature_measurement(self, sensor_id):
-        self.mqtt_handler.publish(f'sensor/{sensor_id}/cmd', self.commands['CMD_GET_TEMPERATURE_DATA'])
+        self.client.publish(f'sensor/{sensor_id}/cmd', self.commands['CMD_GET_TEMPERATURE_DATA'])
+
+    def loop_start(self):
+        self.client.loop_start()
+
+    def loop_stop(self):
+        self.client.loop_stop()
--- mqtt_teleg.py
+++ mqtt_teleg.py
@@ -2,32 +2,37 @@ from time import sleep
 import threading
 from typing import Any, List
 import sqlite3
+import datetime
 
-from modules.mqtt_handler import MQTT_handler as MQTT_handler
-from modules.cmd_handler import Command_Handler as Command_Handler
+from modules.cmd_handler import MQTTCommandHandler as MQTTCommandHandler
 from modules.sensor_container import SensorContainer as SensorContainer
+from modules.meas_logger import MeasurementContainer as MeasurementContainer
 
+def timestamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 if __name__ == '__main__':
 
-    mqtt_handler = MQTT_handler()
-    mqtt_handler.connect()
-    mqtt_handler.subscribe('esp32/frq')
-    mqtt_handler.subscribe('sensor/#')
-    mqtt_handler.publish('esp32/frq', 'Hello World')
+    mqtt_handler = MQTTCommandHandler()
 
-    mqtt_thread = threading.Thread(target=mqtt_handler.loop_start)
-    mqtt_thread.start()
+    mqtt_handler.publish('sensor/01/data/temp', '42.533')
+    mqtt_handler.publish('sensor/02/data/temp', '42.533')
+    mqtt_handler.publish('sensor/03/cmd', 'get_hum_data')
+    mqtt_handler.publish('sensor/03/status', 'critical_error')
+    mqtt_handler.publish('sensor/04/data/hum', '42.533')
 
-    command_handler = Command_Handler(mqtt_handler)
-    
+    mc = MeasurementContainer()
     sc = SensorContainer()
+
+    sc.set_sensor(1, 'temp', 42.533)
 
     # The main thread can continue doing other things
     while True:
-        command_handler.invoke_humidity_measurment('1')
+        mqtt_handler.invoke_humidity_measurment('1')
+        mqtt_handler.publish('sensor/03/status', timestamp())
+        mqtt_handler.publish('sensor/04/data/hum', 'critical_error')
         print(sc.get_sensor(1))
-        sleep(10)
+        sleep(3)
 
     # If you want to wait for the MQTT thread to finish before exiting the main thread
     mqtt_thread.join()
